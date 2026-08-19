4347fc368a761a861531deee2d31d943d9e193a7
Andres Valenzuela <andresvalenzuela3108@gmail.com>
2026-08-19 17:47:34 -0500

    hola_mundo_manual.py updated

--- hola_mundo_manual.py
+++ hola_mundo_manual.py
@@ -1,8 +1,8 @@
 import os
 
 # Lee la variable de entorno pasada desde GitHub Actions
-lenguaje = os.getenv("LENGUAJE_PARAM", "No se ha definido la variable")
-nombre = os.getenv("NOMBRE_PARAM", "No se ha definido la variable")
+lenguaje = os.getenv("LANGUAGE", "No se ha definido la variable")
+nombre = os.getenv("NOMBRE", "No se ha definido la variable")
 
 print(f"El lenguaje recibido es: {lenguaje}")
 print(f"El nombre recibido es: {nombre}")
